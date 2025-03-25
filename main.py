c1802b8c2d8b1a04d77e45fdff4f45b5870f9a4c
gabiisiani <122684207+gabiisiani@users.noreply.github.com>
2025-03-25 12:42:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hi :)")
+
+
+
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+
+even_sum = sum(num for num in range(start, end + 1) if num % 2 == 0)
+
+print(f"The sum of even numbers from {start} to {end} is {even_sum}")
